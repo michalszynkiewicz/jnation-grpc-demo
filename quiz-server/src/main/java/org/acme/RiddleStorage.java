d3fa9a277b1fbc5f7f276da20edcf010b2165e78
Martin Kouba <mkouba@redhat.com>
2022-06-01 16:28:03 +0200

    RiddleStorage - use PostConstruct callback instead of static init

--- quiz-server/src/main/java/org/acme/RiddleStorage.java
+++ quiz-server/src/main/java/org/acme/RiddleStorage.java
@@ -1,14 +1,18 @@
 package org.acme;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
 public class RiddleStorage {
-    private static final List<Riddle> riddles = new ArrayList<>();
 
-    static {
+    private List<Riddle> riddles;
+
+    @PostConstruct
+    void init() {
+        riddles = new ArrayList<>();
         riddles.add(new Riddle("How much is 2+3?", "5", "1", "6"));
         riddles.add(new Riddle("What is #000000 in RGB", "black", "white", "yellow"));
         riddles.add(new Riddle("How tall is Jack Reacher", "195", "180", "200"));
