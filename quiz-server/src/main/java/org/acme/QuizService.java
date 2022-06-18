04555b7df87862df9f65966bf298a63627008d4b
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2022-06-18 10:13:00 +0200

    start

--- quiz-server/src/main/java/org/acme/QuizService.java
+++ quiz-server/src/main/java/org/acme/QuizService.java
@@ -3,29 +3,22 @@ package org.acme;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
-import io.smallrye.mutiny.operators.multi.processors.BroadcastProcessor;
 import io.smallrye.mutiny.operators.multi.processors.UnicastProcessor;
 import io.vertx.core.Vertx;
-
 import org.acme.quiz.grpc.Answer;
 import org.acme.quiz.grpc.Empty;
 import org.acme.quiz.grpc.Question;
 import org.acme.quiz.grpc.Quiz;
 import org.acme.quiz.grpc.Result;
-import org.acme.quiz.grpc.Scores;
-import org.acme.quiz.grpc.SignUpRequest;
-import org.acme.quiz.grpc.SignUpResponse;
-import org.acme.quiz.grpc.UserScore;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.inject.Inject;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicReference;
-
-import javax.inject.Inject;
 
 @GrpcService
 public class QuizService implements Quiz {
@@ -36,8 +29,6 @@ public class QuizService implements Quiz {
     private final Multi<Question> questions = Multi.createBy().replaying()
             .upTo(1).ofMulti(questionSendOut)
             .broadcast().toAllSubscribers();
-
-    private final BroadcastProcessor<Scores> scoreBroadcast = BroadcastProcessor.create();
 
     private final Map<String, Integer> pointsByUser = new ConcurrentHashMap<>();
     private final Set<String> usersWithAnswer = Collections.newSetFromMap(new ConcurrentHashMap<>());
@@ -72,21 +63,6 @@ public class QuizService implements Quiz {
                 Result.newBuilder().setStatus(status).build());
     }
 
-    @Override
-    public Uni<SignUpResponse> signUp(SignUpRequest request) {
-        SignUpResponse.Status status;
-        if (pointsByUser.putIfAbsent(request.getName(), 0) != null) {
-            status = SignUpResponse.Status.NAME_TAKEN;
-        } else {
-            status = SignUpResponse.Status.OKAY;
-        }
-        return Uni.createFrom().item(SignUpResponse.newBuilder().setStatus(status).build());
-    }
-
-    @Override
-    public Multi<Scores> watchScore(Empty request) {
-        return scoreBroadcast;
-    }
 
     @Override
     public Multi<Question> getQuestions(Empty request) {
@@ -101,7 +77,6 @@ public class QuizService implements Quiz {
     public void endQuiz() {
         questionSendOut.onNext(Question.newBuilder().setText("That's all, thanks for playing!").build());
         currentRiddle.set(null);
-        broadcastResults();
     }
 
     void broadcastQuestion(int i) {
@@ -111,17 +86,10 @@ public class QuizService implements Quiz {
         } else {
             questionSendOut.onNext(riddle.toQuestion());
             currentRiddle.set(riddle);
-            broadcastResults();
             usersWithAnswer.clear();
             vertx.setTimer(delay.toMillis(), ignored -> broadcastQuestion(i + 1));
         }
 
     }
 
-    private void broadcastResults() {
-        Scores.Builder scores = Scores.newBuilder();
-        pointsByUser
-                .forEach((user, points) -> scores.addResults(UserScore.newBuilder().setUser(user).setPoints(points).build()));
-        scoreBroadcast.onNext(scores.build());
-    }
 }
