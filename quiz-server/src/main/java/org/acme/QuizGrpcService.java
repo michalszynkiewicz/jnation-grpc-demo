3d4662342fdff5bc3a072d2410de4a6f02c74f97
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2022-06-21 09:40:48 +0200

    start

--- quiz-server/src/main/java/org/acme/QuizGrpcService.java
+++ quiz-server/src/main/java/org/acme/QuizGrpcService.java
@@ -3,15 +3,7 @@ package org.acme;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
-import org.acme.quiz.grpc.Answer;
-import org.acme.quiz.grpc.Empty;
-import org.acme.quiz.grpc.Question;
-import org.acme.quiz.grpc.Quiz;
-import org.acme.quiz.grpc.Result;
-import org.acme.quiz.grpc.Scores;
-import org.acme.quiz.grpc.SignUpRequest;
-import org.acme.quiz.grpc.SignUpResponse;
-import org.acme.quiz.grpc.UserScore;
+
 import org.acme.riddle.Riddle;
 import org.acme.riddle.RiddleService;
 import org.acme.score.ScoreService;
